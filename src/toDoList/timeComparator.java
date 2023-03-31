1fcbf9b168fea13c104ad83da4db7066fc4d42a5
Leon Zuev <loonatune@gmail.com>
2023-04-01 01:19:12 +0300

    Add timeComparator

---
+++ src/toDoList/timeComparator.java
@@ -0,0 +1,16 @@
+package toDoList;
+package src;
+
+import java.util.Comparator;
+import java.util.Map;
+
+public class timeComparator implements Comparator<Map.Entry<Integer, RegularTask>> {
+
+  @Override
+  public int compare(Map.Entry<Integer, RegularTask> o1, Map.Entry<Integer, RegularTask> o2) {
+    if (o1.getValue().getHours() != o2.getValue().getHours()) {
+      return o1.getValue().getHours() - o2.getValue().getHours();
+    }
+    return o1.getValue().getMinutes() - o2.getValue().getMinutes();
+  }
+}
