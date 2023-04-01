f14598ef95a033c55880bf10a4d1ab50f28732e8
Leon Zuev <loonatune@gmail.com>
2023-04-01 13:50:15 +0300

    Target screen with MouseInfo

--- src/interfaces/MainInterface.java
+++ src/interfaces/MainInterface.java
@@ -23,12 +23,27 @@ public class MainInterface {
 
     //определение монитора с активным приложением
     GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-    GraphicsDevice activeScreen = ge.getDefaultScreenDevice();
-    Rectangle screenBounds = activeScreen.getDefaultConfiguration().getBounds();
+    GraphicsDevice[] screenDevices = ge.getScreenDevices();
+    PointerInfo pointerInfo = MouseInfo.getPointerInfo();
+    Point pointerLocation = pointerInfo.getLocation();
+    GraphicsDevice targetScreen = null;
+
+    for (GraphicsDevice screenDevice : screenDevices) {
+      Rectangle screenBounds = screenDevice.getDefaultConfiguration().getBounds();
+      if (screenBounds.contains(pointerLocation)) {
+        targetScreen = screenDevice;
+        break;
+      }
+    }
+    if (targetScreen == null) {
+      targetScreen = ge.getDefaultScreenDevice();
+    }
+
+    Rectangle screenBounds = targetScreen.getDefaultConfiguration().getBounds();
 
     // установка позиции окна на активном мониторе
-     frame.setLocation(screenBounds.x + screenBounds.width / 2 - frame.getWidth() / 2,
-             screenBounds.y + screenBounds.height / 2 - frame.getHeight() / 2);
+    frame.setLocation(screenBounds.x + screenBounds.width / 2 - frame.getWidth() / 2,
+            screenBounds.y + screenBounds.height / 2 - frame.getHeight() / 2);
 
     JPanel panel = new JPanel();
     panel.setLayout(new GridLayout(7, 1));
